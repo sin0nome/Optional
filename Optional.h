85cdbfe887ff28bee2c953b4ccaf8e491e8f5702
sin0nome <radioisotope_thermoelectric@yahoo.co.jp>
2022-06-03 20:13:13 +0900

    戻り値の型が違ったので修正

--- Optional.h
+++ Optional.h
@@ -1,36 +1,33 @@
 #pragma once
 /*
-* 
+*
 *
 * 参考:
 * https://docs.oracle.com/javase/jp/8/docs/api/java/util/Optional.html
 */
 
-
-class NoSuchElementException {
-
-public:
-	NoSuchElementException() = default;
-
-};
-
 template <class T>
 class Optional {
 private:
 	T* pObj = nullptr;	// ポインタ型の値
-	T obj = NULL;		// ポインタ型以外の値
+	T obj = 0;			// ポインタ型以外の値
 
 public:
 
 	explicit Optional(const T val) {
 		this->obj = val;
-		this->pObj = &val;
+		this->pObj = new T;
 	}
 	explicit Optional(const T* val) {
 		this->pObj = val;
 	}
 	explicit Optional(void) = default;
-
+	virtual ~Optional(void) {
+		if (this->pObj != nullptr) {
+			delete this->pObj;
+			this->pObj = nullptr;
+		}
+	}
 
 	// 指定された値がnullでない場合はその値を記述するOptionalを返し、それ以外の場合は空のOptionalを返します。
 	static Optional<T> ofNullable(const T value) {
@@ -38,7 +35,7 @@ public:
 		return optional;
 	}
 	static Optional<T*> ofNullable(const T* value) {
-		if (value == nullptr) {		
+		if (value == nullptr) {
 			return Optional::empty();
 		}
 
@@ -56,24 +53,24 @@ public:
 	// このOptionalに値が存在する場合は値を返し、それ以外の場合はNoSuchElementExceptionをスローします。
 	Optional<T> get(void) const noexcept(false) {
 		if (this->obj == 0 && this->pObj == nullptr) {
-			throw NoSuchElementException;
+			//throw NoSuchElementException;
 		}
-		return this->obj;
+		return this->pObj(this->obj);
 	}
 
 	// このOptionalに値が存在する場合は値を返し、それ以外の場合はNoSuchElementExceptionをスローします。
 	Optional<T*> get(void) noexcept(false) {
 		if (this->obj == 0 && this->pObj == nullptr) {
-			throw NoSuchElementException;
+			//throw NoSuchElementException;
 		}
-		return this->obj;
+		return Optional<T*>(this->pObj);
 	}
 
 	// 値が存在しないtrueを返し、それ以外の場合はfalseを返す。
 	bool isEmpty(void) const {
 		return (this->obj == 0 && this->pObj == nullptr);
 	}
-	
+
 	// 値が存在する場合はtrueを返し、それ以外の場合はfalseを返す。
 	bool isPresent(void) {
 		return (this->obj != 0 || this->pObj != nullptr);
